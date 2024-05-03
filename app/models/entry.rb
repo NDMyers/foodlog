e13610e0e7f4f73ea255e1268346bba1d38c6b64
Nick Myers <ndm81001@gmail.com>
2024-05-03 12:54:15 -0700

    dev branch ready v1.0

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,7 @@
 class Entry < ApplicationRecord
     belongs_to :category
     validates :calories, :protein, :carbohydrates, :fats, :meal_type, :category_id, presence: true
+    validates :calories, :protein, :carbohydrates, :fats, numericality: { greater_than_or_equal_to: 0 }
 
     def day
         self.created_at.strftime("%b %e, %Y")
